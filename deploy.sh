5ec3a9a58ebdb8d6f3d77d1ba8cd7ae7ddaf3930
sswq2020 <a309108243@qq.com>
2021-11-22 22:36:33 +0800

    first commit

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+npm run build
+
+cd public
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:sswq2020/sswq2020.github.io.git master
+
+cd -
